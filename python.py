3bfb04a880fcf3f74398a755bd2365efeff1adc2
gojo-2405 <anonymoushacker24110509@gmail.com>
2026-02-19 11:00:30 +0530

    trigger python.py

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 # trigger_test.py
 import datetime
 
-print("Hello Jenkins!")
+print("Hello Karthick!")
 print("This is a Poll SCM trigger test.")
 print("Build triggered at:", datetime.datetime.now())
