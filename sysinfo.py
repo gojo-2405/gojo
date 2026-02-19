6d1fc226ed53d904be09981af5352b1dd4ceae11
gojo-2405 <anonymoushacker24110509@gmail.com>
2026-02-19 11:10:43 +0530

    sys

---
+++ sysinfo.py
@@ -0,0 +1,6 @@
+# system_info.py
+import platform
+
+print("Jenkins Trigger System Info Test")
+print("Python version:", platform.python_version())
+print("Operating system:", platform.system(), platform.release())
